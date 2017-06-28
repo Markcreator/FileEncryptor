9bcd27f3e3583fcdabbc70c9dbbe6330a0c3ba45
Mark Horn <markmediahorn@hotmail.com>
2017-06-28 14:45:33 +0200

    Added AESKey, started on EncryptorService

---
+++ src/me/Markcreator/FileEncryptor/AESKey.java
@@ -0,0 +1,31 @@
+package me.Markcreator.FileEncryptor;
+
+import java.security.MessageDigest;
+import java.util.Arrays;
+
+import javax.crypto.spec.SecretKeySpec;
+
+public class AESKey {
+
+	private SecretKeySpec keySpec;
+	
+	public AESKey(String key) {
+		byte[] keyBytes = key.getBytes();
+		
+		// Build AES key
+		try {
+			MessageDigest sha = MessageDigest.getInstance("SHA-1");
+			keyBytes = sha.digest(keyBytes);
+			keyBytes = Arrays.copyOf(keyBytes, 16);
+	
+			keySpec = new SecretKeySpec(keyBytes, "AES");
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public SecretKeySpec getKeySpec() {
+		return keySpec;
+	}
+}
--- src/me/Markcreator/FileEncryptor/Main.java
+++ src/me/Markcreator/FileEncryptor/Main.java
@@ -1,42 +1,29 @@
 package me.Markcreator.FileEncryptor;
 
-import java.security.MessageDigest;
-import java.util.Arrays;
-
 import javax.crypto.Cipher;
-import javax.crypto.spec.SecretKeySpec;
 
 public class Main {
 
 	public static void main(String[] args) {
 		try {
 			// Info
-			byte[] key = "key".getBytes(); 
-			byte[] input = "test".getBytes();
-			byte[] output = null;
-			byte[] decrypted = null;
+			String input = "test";
+			String output = null;
+			String decrypted = null;
 			
 			// Build AES key
-			MessageDigest sha = MessageDigest.getInstance("SHA-1");
-			key = sha.digest(key);
-			key = Arrays.copyOf(key, 16);
-			
-			SecretKeySpec keySpec = null;
-			keySpec = new SecretKeySpec(key, "AES");
+			AESKey key = new AESKey("key");
 			
 			// Encrypt
-			Cipher cipher = Cipher.getInstance("AES");
-			cipher.init(Cipher.ENCRYPT_MODE, keySpec);
-			output = cipher.doFinal(input);
-			
+			output = EncryptorService.transcode(Cipher.ENCRYPT_MODE, input, key);
+
 			// Decrypt
-			cipher.init(Cipher.DECRYPT_MODE, keySpec);
-		    decrypted = cipher.doFinal(output);
-		    
-		    System.out.println("Original: " + new String(input));
+			decrypted = EncryptorService.transcode(Cipher.DECRYPT_MODE, output, key);
+
+			System.out.println("Original: " + new String(input));
 			System.out.println("Encrypted: " + new String(output));
-		    System.out.println("Decrypted: " + new String(decrypted));
-			
+			System.out.println("Decrypted: " + new String(decrypted));
+
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
